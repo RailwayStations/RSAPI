c01d0b2df15bbbf16b22e9526a64bbd0e3a5c0fa
Peter Storch <peter.storch@tuta.io>
2022-08-02 23:15:45 +0200

    Make test more explizit

--- src/test/java/org/railwaystations/rsapi/core/services/InboxServiceTest.java
+++ src/test/java/org/railwaystations/rsapi/core/services/InboxServiceTest.java
@@ -10,28 +10,28 @@ import static org.assertj.core.api.Assertions.assertThat;
 class InboxServiceTest {
 
     @Test
-    void getLicenseNoOverride() {
-        assertThat(InboxService.getLicenseForPhoto(createUserWithCC0License(),
-                Country.builder()
-                        .code("de")
-                        .build()))
-                .isEqualTo(License.CC0_10);
+    void licenseOfPhotoShouldBeTheLicenseOfUser() {
+        var licenseForPhoto = InboxService.getLicenseForPhoto(createUserWithCC0License(), createCountryWithOverrideLicense(null));
+        assertThat(licenseForPhoto).isEqualTo(License.CC0_10);
+    }
+
+    @Test
+    void licenseOfPhotoShouldBeOverridenByLicenseOfCountry() {
+        var licenseForPhoto = InboxService.getLicenseForPhoto(createUserWithCC0License(), createCountryWithOverrideLicense(License.CC_BY_NC_SA_30_DE));
+        assertThat(licenseForPhoto).isEqualTo(License.CC_BY_NC_SA_30_DE);
+    }
+
+    private Country createCountryWithOverrideLicense(License overrideLicense) {
+        return Country.builder()
+                .code("xx")
+                .overrideLicense(overrideLicense)
+                .build();
     }
 
     private User createUserWithCC0License() {
         return User.builder()
                 .license(License.CC0_10)
                 .build();
-    }
-
-    @Test
-    void getLicenseOverride() {
-        assertThat(InboxService.getLicenseForPhoto(createUserWithCC0License(),
-                Country.builder()
-                        .code("fr")
-                        .overrideLicense(License.CC_BY_NC_SA_30_DE)
-                        .build()))
-                .isEqualTo(License.CC_BY_NC_SA_30_DE);
     }
 
 }
