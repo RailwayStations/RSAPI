83439494ecd6c3dbdbb0dd068e25ee6045026668
Peter Storch <peter.storch@tuta.io>
2024-05-20 20:49:59 +0200

    migrate to flyway

---
+++ service/src/test/resources/testdata/providerApps.sql
@@ -0,0 +1,10 @@
+INSERT INTO providerApps (countryCode,`type`,name,url) VALUES
+	 ('ch','android','SBB Mobile','https://play.google.com/store/apps/details?id=ch.sbb.mobile.android.b2c'),
+	 ('ch','ios','SBB Mobile','https://apps.apple.com/app/sbb-mobile/id294855237'),
+	 ('de','android','DB Navigator','https://play.google.com/store/apps/details?id=de.hafas.android.db'),
+	 ('de','android','FlixTrain','https://play.google.com/store/apps/details?id=de.meinfernbus'),
+	 ('de','ios','DB Navigator','https://apps.apple.com/app/db-navigator/id343555245'),
+	 ('it','android','Trenitalia','https://play.google.com/store/apps/details?id=com.lynxspa.prontotreno'),
+	 ('it','ios','Trenitalia','https://apps.apple.com/ch/app/trenitalia/id331050847'),
+	 ('se','android','SJ - Biljetter och trafikinfo','https://play.google.com/store/apps/details?id=se.sj.android'),
+	 ('se','ios','SJ - Biljetter och trafikinfo','https://apps.apple.com/de/app/sj-biljetter-och-trafikinfo/id442994825');
